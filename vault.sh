ccff1e2f09c09fe58caa7cd7bef769ca065a9917
troy600 <bazartroy531@gmail.com>
2025-01-02 17:10:44 +0800

    hardly coded

--- vault.sh
+++ vault.sh
@@ -1,1 +1,33 @@
 #!/usr/bin/bash
+
+Foldermake() {
+    mkdir ~/.config/secrets -p
+}
+
+CheckFIle() {
+    echo null
+}
+
+EncryptFile() {
+    echo
+}
+
+DecryptFile() {
+    expect "this"
+    send "asaas"
+}
+
+SelectFile() {
+    echo null
+}
+
+# shellcheck disable=SC1073
+if [[ ! -f "$HOME/.config/secrets" ]]; then 
+    Foldermake
+fi
+    
+
+
+#zenity --title "select a folder to encrypt your file >:)" --file-selection --directory
+
+#exec ./ssss.exp bazar124
